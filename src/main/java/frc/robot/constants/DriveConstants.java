0a7bbf54541ff7b831046eaffe26e877322c6078
martinying <5122842+martinying@users.noreply.github.com>
2025-10-22 20:57:04 -0700

    Add Gear ratio info

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -4,5 +4,9 @@ public class DriveConstants {
 
     public static final int IMU_CAN_ID = 0; //CAN
 
-    public static final double MAX_SPEED_METER_PER_SECCONDS_DEFAULT_VALUE = 5;
+    public static final double MAX_SPEED_METER_PER_SECONDS_DEFAULT_VALUE = 5;
+
+    //GEAR RATIO IS PULLED FROM SITE BELOW
+    //https://www.swervedrivespecialties.com/products/mk4i-swerve-module
+    public static final double SWERVE_MODULE_DRIVE_MOTOR_GEAR_RATIO = 6.25;
 }
